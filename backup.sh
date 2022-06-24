00d31b1d9bfab74af41a22c4c61f2f29bdc33337
Haunui Saint-sevin <haunui@saint-sevin.fr>
2022-06-24 14:48:39 +0200

    Specify 'StrictHostKeyChecking=no' for rsync

--- backup.sh
+++ backup.sh
@@ -29,5 +29,5 @@ else
 fi
 
 
-rsync -az "$tmp_bkp_path/*" $BKP_SSH_LOGIN:/volume1/aws-bkp/
+rsync -e "ssh -o StrictHostKeyChecking=no" -az "$tmp_bkp_path/*" $BKP_SSH_LOGIN:/volume1/aws-bkp/
 rm -rf "$tmp_bkp_path" &> /dev/null
